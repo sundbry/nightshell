4c2e61876122ada8529ee92924a29d59ff2cefb9
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-08-19 12:23:38 -0700

    Upgrade to clojure 1.7.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject sundbry/nightshell "0.1.3"
+(defproject sundbry/nightshell "0.1.4"
   :description "A debug repl derived from Nightcode and REDL"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [seesaw "1.4.5"]
                  [com.fifesoft/rsyntaxtextarea "2.5.6"]
                  [com.github.insubstantial/substance "7.3"]
-                 [clj-stacktrace "0.2.7"]]
+                 [clj-stacktrace "0.2.8"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :aot :all
