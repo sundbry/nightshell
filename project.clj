45f10360eff75a7d8fbe331ae4a3c6570985aeb2
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-11-20 23:10:55 -0800

    Upgrade core.async

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject sundbry/nightshell "0.1.4"
+(defproject sundbry/nightshell "0.1.5"
   :description "A debug repl derived from Nightcode and REDL"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.371"]
                  [seesaw "1.4.5"]
                  [com.fifesoft/rsyntaxtextarea "2.5.6"]
                  [com.github.insubstantial/substance "7.3"]
