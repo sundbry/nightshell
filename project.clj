7211c4553b86a7717e39dbe1543189ddb83786ce
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-11-20 23:11:34 -0800

    Upgrade deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject sundbry/nightshell "0.1.5"
   :description "A debug repl derived from Nightcode and REDL"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.371"]
+                 [org.clojure/core.async "0.2.374"]
                  [seesaw "1.4.5"]
-                 [com.fifesoft/rsyntaxtextarea "2.5.6"]
+                 [com.fifesoft/rsyntaxtextarea "2.5.8"]
                  [com.github.insubstantial/substance "7.3"]
                  [clj-stacktrace "0.2.8"]]
   :source-paths ["src/clj"]
