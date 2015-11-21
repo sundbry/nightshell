0b5e3c4ce0f2ff29f84cac37e904b2856dfb49db
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-11-20 23:21:40 -0800

    Fix tests more

--- test/nightshell/test.clj
+++ test/nightshell/test.clj
@@ -1,7 +1,7 @@
 (ns nightshell.test
   (:require 
     [clojure.test :refer :all]
-    [nightshell.core :refer [break catch-break]]))
+    [nightshell.core :refer [break breakpoint catch-break]]))
 
 (defn with-enabled
   [test-fn]
@@ -17,7 +17,7 @@
 
 (defn fail
   []
-  (throw (ex-info "This function FAILS! It's becoming an epic failure!" {})))
+  (throw (ex-info "This function FAILS!" {})))
 
 #_(deftest test-break
   (instruct "Return 1")
@@ -29,17 +29,17 @@
 
 (deftest test-catch-break-through
   (instruct "Nothing should break here.")
-  (is 2 (+ 1 (catch-break 1)))
+  (is (= 2 (+ 1 (catch-break 1))))
   (instruct "Now things are going to start to break... (continue 1)")
-  (is 2 (+ 1 (catch-break (throw (Exception. "You should continue with 1 here"))))))
+  (is (= 2 (+ 1 (catch-break (throw (Exception. "You should continue with 1 here")))))))
 
 (deftest test-break-exception
   (instruct "Breakpoint should catch an exception. Contnue with it.")
-  (is (thrown? clojure.lang.ExceptionInfo (+ 1 (break (fail)))))
+  (is (thrown? clojure.lang.ExceptionInfo (+ 1 (breakpoint (fail)))))
   (instruct "Breakpoint should catch an exception. Contnue with 1 this tme.")
-  (is 2 (+ 1 (break (fail))))
+  (is (= 2 (+ 1 (break (fail)))))
   (instruct "OK that's done"))
 
 #_(deftest test-break-user-exception
     (instruct "Throw an exception in the repl. Contnue with 1")
-    (is 2 (+ 1 (break 1))))
+    (is (= 2 (+ 1 (break 1)))))
