8e4735ceb5d0cdfd09f34293eff73751de3e3f55
Ryan Sundberg <ryan.sundberg@gmail.com>
2016-03-02 14:54:25 -0800

    Remove aot compiling, use profiles for clojure inclusion

--- project.clj
+++ project.clj
@@ -1,12 +1,17 @@
-(defproject sundbry/nightshell "0.1.5"
+(defproject sundbry/nightshell "0.1.6-SNAPSHOT"
   :description "A debug repl derived from Nightcode and REDL"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.374"]
+  :dependencies [[org.clojure/core.async "0.2.374"]
                  [seesaw "1.4.5"]
                  [com.fifesoft/rsyntaxtextarea "2.5.8"]
                  [com.github.insubstantial/substance "7.3"]
                  [clj-stacktrace "0.2.8"]]
+  :profiles
+  {:1.6
+   {:dependencies
+    [[org.clojure/clojure "1.6.0"]]}
+   :1.7
+   {:dependencies
+    [[org.clojure/clojure "1.7.0"]]}}
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
-  :aot :all
-  :main nightshell.core)
+  :main ^:skip-aot nightshell.core)
